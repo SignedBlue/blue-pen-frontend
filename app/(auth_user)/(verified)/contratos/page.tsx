f0a739ce4414106b69885fd065ded10f0dbad888
marlonberaldo <marlon_beraldo@hotmail.com>
2023-11-20 16:59:43 -0300

    🔒️ Fix: remove isAdmin from client contractList

--- app/(auth_user)/(verified)/contratos/page.tsx
+++ app/(auth_user)/(verified)/contratos/page.tsx
@@ -20,7 +20,7 @@ export default async function UserContractsPage() {
 
   return (
     <Suspense fallback={<LoadingSkeleton />}>
-      <ContractList isAdmin newContract contracts={contracts} />
+      <ContractList contracts={contracts} />
     </Suspense>
   );
 }
